3567f041d6e667bdfa12530e42b14082fafcdd46
Tu Dinh <tu.dinh@metropolia.fi>
2025-02-27 13:32:18 +0200

    Initial commit

---
+++ src/main/java/TimeCal.java
@@ -0,0 +1,9 @@
+public class TimeCal {
+    public static void main(String[] args) {
+        System.out.println("Time needs " + timeCalculator(100, 100));
+    }
+
+    public static double timeCalculator(double x, double v) {
+        return x/v;
+    }
+}
---
+++ src/test/java/TimeCalTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeCalTest {
+
+    @Test
+    void timeCalculator() {
+        assertEquals(1.0, TimeCal.timeCalculator(100, 100));
+    }
+}
