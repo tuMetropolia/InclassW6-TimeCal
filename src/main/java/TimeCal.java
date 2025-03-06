bd66d87c5a00bccd6b376de600c1646155513e90
Tu Dinh <tu.dinh@metropolia.fi>
2025-03-06 13:07:10 +0200

    commit

--- src/main/java/TimeCal.java
+++ src/main/java/TimeCal.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class TimeCal {
     public static void main(String[] args) {
         System.out.println("Time needs " + timeCalculator(100, 100));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the distance: ");
+        double x = sc.nextDouble();
+        System.out.println("Enter the velocity: ");
+        double v = sc.nextDouble();
+        System.out.println("Time needs " + timeCalculator(x, v));
     }
 
     public static double timeCalculator(double x, double v) {
